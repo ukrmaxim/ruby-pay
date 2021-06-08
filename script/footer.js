c1b6aa0116f04c486214ed97f82bfe3cdf1023a7
ukrmaxim <ukrmaxim@protonmail.com>
2021-06-08 12:34:32 +0300

    Add jQuery

---
+++ script/footer.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.footer-nav-link').click(function(){
+    var id = $(this).attr('href');
+
+    $('html, body').animate({
+      scrollTop: ($(id).offset().top - 76)
+    }, 500);
+    return false;
+  });
+});
